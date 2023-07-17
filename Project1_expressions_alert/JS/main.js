e4f14b67b46bedfe56f02f0652fe1b569b8316d4
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-17 18:19:44 +0100

    Cloned new path
    Technical hiccups caused commits to stop showing, trying to fix that by cloning a new path to repository

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,3 +1,17 @@
-document.write(A);
+//var A = "Creating a string";
 
-var A = "Creating a variable using document.write";
+//document.write(A);
+
+//document.write("This is a sample, \'you can't take away my dignity\'");
+
+//document.write("\"If you don\'t like the road you\'re walking, start paving another one!\""
+//+ "— Dolly Parton");
+
+//var B = ("\"If you don\'t like the road you\'re walking, start paving another one!\""
+//+ " Dolly Parton") + " - 2014";
+//document.write(B)
+
+//var fAmily = "Houston", fAther = "John", mOther = "Cissy", dAughter = "Whitney";
+//document.write(dAughter);
+
+document.write(9 + 9);

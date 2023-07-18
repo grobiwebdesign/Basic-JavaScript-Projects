c798bcd9aed7d84a4023864bc7422b156f2e1880
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-18 20:32:18 +0100

    Functions Assignment
    Applying comments to further explain what JS is doing, also added a cursor pointer for user friendliness

--- project2_functions/JS/main.js
+++ project2_functions/JS/main.js
@@ -8,8 +8,8 @@ function project2_Function(){
 }
 */
 
-function conCatenated(){
-    var paragraph = "This is...";
-    paragraph += " what an operator string looks like.";
-    document.getElementById("concatenate").innerHTML = paragraph;
+function conCatenated(){ //
+    var paragraph = "This is..."; // this is the variable
+    paragraph += " what an operator string looks like."; // += operator adds additonal words to this variable
+    document.getElementById("concatenate").innerHTML = paragraph; // using getElementByID method to bind function to HTML, which makes words visible in browser window.
 }

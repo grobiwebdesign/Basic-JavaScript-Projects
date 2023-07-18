1d6544fc64e66e380a7887beecb8be5afc4a42f8
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-18 16:26:54 +0100

    Basic Javascript projects assignment
    Completed exercises with minimum project requirements

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -1,8 +1,15 @@
 // All previous exercises are commented out so to show my progress throughout the course
 
-//var A = "Creating a string";
+// window.alert("Hello Ela and Kasia");
 
-//document.write(A);
+// document.write("I love making websites");
+
+//var sent1 = "This is the beginning of the string";
+//var sent2 = "This is the ending of the string";
+
+//document.write(sent1 + sent2);
+
+//document.write(9 + 18);
 
 //document.write("This is a sample, \'you can't take away my dignity\'");
 
@@ -16,11 +23,11 @@
 //var fAmily = "Houston", fAther = "John", mOther = "Cissy", dAughter = "Whitney";
 //document.write(dAughter);
 
-//document.write(9 + 18);
+
 
 setInterval(theTime, 1000); // Executing a function and indicating length of time-interval (ms)
 
-function theTime() { // named function called 'theTime'
-  const d = new Date(); // Calling a constant
+function theTime() { // created theTime() function to display the time in the HTML doc
+  const d = new Date(); // created a const variable to hold the Date() object
   document.getElementById("time").innerHTML = d.toLocaleTimeString(); // Placing value of constant into HTML element
 } // Close function ** example courtesy of W3schools.com

d4bbbb6999ef80358096e9a6c3f576c6e23ee54b
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-18 20:22:56 +0100

    Operator assignment
    using += operator to concatenate a string

--- project2_functions/JS/main.js
+++ project2_functions/JS/main.js
@@ -1,8 +1,15 @@
+/*
 function project2_Function(){
+
     var one = "Foo";
     var two = "Bar";
 
     document.getElementById("project2").innerHTML = one + two;
-
 }
+*/
 
+function conCatenated(){
+    var paragraph = "This is...";
+    paragraph += " what an operator string looks like.";
+    document.getElementById("concatenate").innerHTML = paragraph;
+}

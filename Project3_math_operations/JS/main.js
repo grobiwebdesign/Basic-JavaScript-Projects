75abcc2c571de87cdc7f160c2c715871361277d2
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-20 13:52:45 +0100

    Multiplication and Division Assignment
    Completed tasks involving multiplying and dividing

--- Project3_math_operations/JS/main.js
+++ Project3_math_operations/JS/main.js
@@ -2,3 +2,18 @@ function addIt_on(){
     var result = 5 + 5;
     document.getElementById("Math").innerHTML = result;
 }
+
+function takeIt_off(){
+    var result = 5 - 5;
+    document.getElementById("Math").innerHTML = result;
+}
+
+function multiPly(){
+    var result = 5 * 5;
+    document.getElementById("Math").innerHTML = result;
+}
+
+function divIde(){
+    var result = 5 / 5;
+    document.getElementById("Math").innerHTML = result;
+}

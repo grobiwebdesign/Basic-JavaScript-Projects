3c7034bb066292961de04e11a1bb818a944896e6
Graeme Robinson <grobiwebdesign@gmail.com>
2023-07-23 12:25:23 +0100

    Project 5 Comparisons assignment
    Use document.write() method to display data type
    
    Write expression combining strings and numbers
    
    Use various operators covered in this module

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,34 @@
+// Create a Boolean
+var X = true;
+
+// Print data type of Boolean
+document.write(typeof X);
+
+// string with number
+document.write("10" + 5);
+
+// document.write( 10 > 4 );
+
+// console.log( 5 < 4 );
+
+//document.getElementById("demo").innerHTML = isNaN('not a number');
+
+// Initate is-equal-to ( == ) operator
+document.write( 41 == 41 );
+
+/*
+X = 10
+Y = "Four"
+document.write(X === Y);
+*/
+
+// Initiate a greater than ( > ) and OR ( || ) operator 
+document.write(5 > 10 || 10 > 20);
+
+// Initiate locigal NOT ( ! ) operator
+function not_function(){
+
+  document.getElementById("demo").innerHTML = !(20 > 10);
+
+}
+

50ed3f793182f805c8292c593a8412d1089fd86d
Graeme Robinson <grobiwebdesign@gmail.com>
2023-08-09 20:13:21 +0100

    Project9_Countdown_Slideshow Assignment
    Create a countdown() function and basic slideshow

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,16 +1,16 @@
 // Countdown Assignment
 
-function countDown(){
-  var seconds = document.getElementById("seconds").value;
+function countDown(){ // name of function for countdown timer 
+  var seconds = document.getElementById("seconds").value; // captures user input
 
-  function tick(){
+  function tick(){ // mechanics of countdown function
     seconds = seconds - 1;
     timer.innerHTML = seconds;
-    var time = setTimeout(tick, 1000);
-    if (seconds == -1) {
+    var time = setTimeout(tick, 1000); // 100 milliseconds
+    if (seconds == -1) { // stop timer after 1000 milliseconds
       alert("Time's up");
       clearTimeout(time);
-      timer.innerHTML = "";
+      timer.innerHTML = ""; 
     }
   }
   tick();
@@ -27,19 +27,20 @@ function arrowButton(n) {
   show_Slides(image_Library += n);
 }
 
-// thumbnail
+// show thumbnail
 function currentSlide(n) {
   show_Slides(image_Library = n)
 }
 
+// slideshow mechanics
 function show_Slides(n) {
   let i;
-  let slides = document.getElementByClassName("slide");
+  let slides = document.getElementsByClassName("slide");
   if (n > slides.length) {image_Library = 1}
   if (n < 1) {image_Library = slides.length}
-    for i = 0; i < slides.length i++ {
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
 
-    slides[image_Library-1].style.display = "block";
+    slides[image_Library-1].style.display = "block"; // disables the "display:none; css property " for corresponding image
 }
